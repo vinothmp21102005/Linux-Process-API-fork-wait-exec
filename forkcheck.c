047ee60a9205db7a6cd7dcfb90e0aeb615297a1b
vinothmp21102005 <mpvinoth2180@gmail.com>
2024-03-05 21:37:21 +0530

    exp2

--- forkcheck.c
+++ forkcheck.c
@@ -1,1 +1,15 @@
 //C Program to create new process using Linux API system calls fork() and exit()
+#include <stdio.h>
+#include<stdlib.h>
+int main()
+{ int pid; 
+pid=fork(); 
+if(pid == 0) 
+{ printf("Iam child my pid is %d\n",getpid()); 
+printf("My parent pid is:%d\n",getppid()); 
+exit(0); } 
+else{ 
+printf("I am parent, my pid is %d\n",getpid()); 
+sleep(100); 
+exit(0);} 
+}
